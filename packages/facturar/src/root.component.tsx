1064e13d4209facf8842bfafeff9c55cde209e2c
Fabian Novoa <fabianleandronovoa@gmail.com>
2024-06-19 09:38:03 -0500

    Prueba taildwind

--- packages/facturar/src/root.component.tsx
+++ packages/facturar/src/root.component.tsx
@@ -1,3 +1,3 @@
 export default function Root(props) {
-  return <section className="bg-white" >{props.name} prueba de estilo!</section>;
+  return <section className="bg-white text-3xl text-red-600" >{props.name} prueba de estilo!</section>;
 }
